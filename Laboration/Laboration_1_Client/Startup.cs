d558a42333e53027245aa9b2d911ad6a3f1161c4
Nelly Haglund <haglundnelly@gmail.com>
2016-01-12 11:46:43 +0100

    added Laboration solution with projects for lab 1

---
+++ Laboration/Laboration_1_Client/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Laboration_1_Client.Startup))]
+namespace Laboration_1_Client
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
