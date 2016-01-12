92191ce2228e9f65644724ca5c397048773825c4
Nelly Haglund <haglundnelly@gmail.com>
2016-01-12 14:12:03 +0100

    Added projekt for laboration 1.2

---
+++ Laboration/Laboration_1_Punkt_2_Service/AddTwoNumbers.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="AddTwoNumbers.asmx.cs" Class="Laboration_1_Punkt_2_Service.AddTwoNumbers" %>
---
+++ Laboration/Laboration_1_Punkt_2_Service/AddTwoNumbers.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Laboration_1_Punkt_2_Service
+{
+    /// <summary>
+    /// Summary description for AddTwoNumbers
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class AddTwoNumbers : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
