7e19edc3bd907deb74b9ce3a70139fe24099c179
Nelly Haglund <haglundnelly@gmail.com>
2016-01-12 14:43:09 +0100

    Added AddThreeNumbersService with method to add three numbers

---
+++ Laboration/Laboration_1_Punkt_3_Service/AddThreeNumbersService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="AddThreeNumbersService.asmx.cs" Class="Laboration_1_Punkt_3_Service.AddThreeNumbersService" %>
---
+++ Laboration/Laboration_1_Punkt_3_Service/AddThreeNumbersService.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Laboration_1_Punkt_3_Service
+{
+    /// <summary>
+    /// Summary description for AddThreeNumbersService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class AddThreeNumbersService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string AddThreeNumbers(string x, string y, string z)
+        {
+            return (float.Parse(x) + float.Parse(y) + float.Parse(z)).ToString();
+        }
+    }
+}
