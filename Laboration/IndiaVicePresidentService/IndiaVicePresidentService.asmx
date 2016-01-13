0d07e71f5c6fbbc144e834d353dcddb3a7c7a4b5
Nelly Haglund <haglundnelly@gmail.com>
2016-01-13 08:02:23 +0100

    Added asmx file for India vice president service with method and logic to return vice president for given year

---
+++ Laboration/IndiaVicePresidentService/IndiaVicePresidentService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="IndiaVicePresidentService.asmx.cs" Class="IndiaVicePresidentService.IndiaVicePresidentService" %>
---
+++ Laboration/IndiaVicePresidentService/IndiaVicePresidentService.asmx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace IndiaVicePresidentService
+{
+    /// <summary>
+    /// Summary description for IndiaVicePresidentService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class IndiaVicePresidentService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string IndiaVicePresidentGivenYear(string year)
+        {
+            var yearInteger = int.Parse(year);
+            var vicePresidents =
+                File.ReadAllLines(
+                    @"C:\Users\NellyHaglund\Documents\Skolan\WCF\Laborationer\WCF_Github\WCF\Laboration\IndiaVicePresidentService\IndiaVicePresidents.txt");
+
+            var message = "";
+
+            foreach (var vicePresident in vicePresidents)
+            {
+                var split = vicePresident.Split(';');
+                var name = split[0];
+                var years = split[1].Split('-');
+                if (yearInteger >= int.Parse(years[0]) && yearInteger <= int.Parse(years[1]))
+                {
+                    message = $"Vice president in India year {yearInteger}: {name}";
+                    break;
+                }
+                else
+                {
+                    message = $"No vice president found in year {yearInteger}";
+                }
+
+            }
+            return message;
+        }
+    }
+}
