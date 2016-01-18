d23dc58b9abf4966646ae4d9daf5c1356f2453f4
Nelly Haglund <haglundnelly@gmail.com>
2016-01-18 15:16:20 +0100

    Laboration 2 - Added Solution for host and service with console projekt for the service

---
+++ Ecutbildning.Laboration2Service/Ecutbildning.Laboration2Service/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecutbildning.Laboration2Service
+{
+    [ServiceContract(Namespace = "http://Ecutbildning.Laboration2Service")]
+    public interface IBMICalculator
+    {
+       
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
