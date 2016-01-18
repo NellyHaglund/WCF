315916843c75efed766f157e4e68f25a70eae782
Nelly Haglund <haglundnelly@gmail.com>
2016-01-18 19:33:00 +0100

    Added components to win form application for BMICalculation

--- Ecutbildning.Client/Ecutbildning.BMIClientForm/Form1.Designer.cs
+++ Ecutbildning.Client/Ecutbildning.BMIClientForm/Form1.Designer.cs
@@ -28,20 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.textBoxWeight = new System.Windows.Forms.TextBox();
+            this.buttonCalc = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Location = new System.Drawing.Point(25, 79);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(100, 22);
+            this.textBoxHeight.TabIndex = 0;
+            this.textBoxHeight.Text = "height";
+            // 
+            // textBoxWeight
+            // 
+            this.textBoxWeight.Location = new System.Drawing.Point(207, 79);
+            this.textBoxWeight.Name = "textBoxWeight";
+            this.textBoxWeight.Size = new System.Drawing.Size(100, 22);
+            this.textBoxWeight.TabIndex = 1;
+            this.textBoxWeight.Text = "weight";
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.Location = new System.Drawing.Point(107, 161);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(127, 30);
+            this.buttonCalc.TabIndex = 2;
+            this.buttonCalc.Text = "Calculate";
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(104, 225);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(73, 17);
+            this.labelResult.TabIndex = 3;
+            this.labelResult.Text = "Your BMI: ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(350, 405);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonCalc);
+            this.Controls.Add(this.textBoxWeight);
+            this.Controls.Add(this.textBoxHeight);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.TextBox textBoxWeight;
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.Label labelResult;
     }
 }
 
