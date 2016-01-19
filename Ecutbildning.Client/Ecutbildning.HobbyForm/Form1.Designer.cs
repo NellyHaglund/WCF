8c427583372d5a601bbfbdc34eb56ca7c1c2ee83
Nelly Haglund <haglundnelly@gmail.com>
2016-01-19 11:22:26 +0100

    Added content to lab 2.4 win form project

--- Ecutbildning.Client/Ecutbildning.HobbyForm/Form1.Designer.cs
+++ Ecutbildning.Client/Ecutbildning.HobbyForm/Form1.Designer.cs
@@ -28,20 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxKilo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxGender = new System.Windows.Forms.TextBox();
+            this.buttonResult = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // textBoxKilo
+            // 
+            this.textBoxKilo.Location = new System.Drawing.Point(82, 107);
+            this.textBoxKilo.Name = "textBoxKilo";
+            this.textBoxKilo.Size = new System.Drawing.Size(239, 22);
+            this.textBoxKilo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(328, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "kg";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(251, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Fyll i hur många kilo du tar i bänkpress";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Är du man eller kvinna?";
+            // 
+            // textBoxGender
+            // 
+            this.textBoxGender.Location = new System.Drawing.Point(82, 195);
+            this.textBoxGender.Name = "textBoxGender";
+            this.textBoxGender.Size = new System.Drawing.Size(239, 22);
+            this.textBoxGender.TabIndex = 4;
+            // 
+            // buttonResult
+            // 
+            this.buttonResult.Location = new System.Drawing.Point(82, 249);
+            this.buttonResult.Name = "buttonResult";
+            this.buttonResult.Size = new System.Drawing.Size(239, 37);
+            this.buttonResult.TabIndex = 5;
+            this.buttonResult.Text = "ÄR JAG STARK?";
+            this.buttonResult.UseVisualStyleBackColor = true;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(82, 317);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 17);
+            this.labelResult.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(570, 441);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonResult);
+            this.Controls.Add(this.textBoxGender);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxKilo);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxKilo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxGender;
+        private System.Windows.Forms.Button buttonResult;
+        private System.Windows.Forms.Label labelResult;
     }
 }
 
