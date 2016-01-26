cfff2439c395d477bc1703505f4386860bb5374f
Nelly Haglund <haglundnelly@gmail.com>
2016-01-26 11:58:15 +0100

    Added functionallity in Get Shipper button event, to get shipper from service

--- Laboration_3/ShipperClient/Default.aspx.cs
+++ Laboration_3/ShipperClient/Default.aspx.cs
@@ -16,11 +16,17 @@ namespace ShipperClient
 
         protected void getShipperButton_Click(object sender, EventArgs e)
         {
+            var host = new ShipperServiceClient();
+            var shipper = host.GetShipperById(int.Parse(TextBoxShipperCode.Text));
 
+            TextBoxCompanyName.Text = shipper.CompanyName;
+            TextBoxPhone.Text = shipper.Phone;
+            TextBoxShipperId.Text = shipper.ShipperID.ToString();
         }
 
         protected void TextBoxPhone_TextChanged(object sender, EventArgs e)
         {
+            var host = new ShipperServiceClient();
 
         }
     }
