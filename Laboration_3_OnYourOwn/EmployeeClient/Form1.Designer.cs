335c07eeb1e01d792cca0936e342c8ebe14adada
Nelly Haglund <haglundnelly@gmail.com>
2016-01-26 14:27:31 +0100

    Added components to windows form client

---
+++ Laboration_3_OnYourOwn/EmployeeClient/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace EmployeeClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.textBoxFirstname = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.textBoxContry = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxEmployeeCode = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.buttonGetEmployee = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(24, 314);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(387, 22);
+            this.textBoxId.TabIndex = 0;
+            // 
+            // textBoxFirstname
+            // 
+            this.textBoxFirstname.Location = new System.Drawing.Point(480, 314);
+            this.textBoxFirstname.Name = "textBoxFirstname";
+            this.textBoxFirstname.Size = new System.Drawing.Size(387, 22);
+            this.textBoxFirstname.TabIndex = 1;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(24, 367);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(387, 22);
+            this.textBoxLastName.TabIndex = 2;
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(480, 367);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(387, 22);
+            this.textBoxTitle.TabIndex = 3;
+            // 
+            // textBoxContry
+            // 
+            this.textBoxContry.Location = new System.Drawing.Point(24, 421);
+            this.textBoxContry.Name = "textBoxContry";
+            this.textBoxContry.Size = new System.Drawing.Size(387, 22);
+            this.textBoxContry.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 291);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Employee ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(477, 294);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Firstname:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 347);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Lastname: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(477, 347);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Title";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 401);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Country:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(138, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Get Employee By ID:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(24, 253);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(260, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Changes current employee information: ";
+            // 
+            // textBoxEmployeeCode
+            // 
+            this.textBoxEmployeeCode.Location = new System.Drawing.Point(24, 74);
+            this.textBoxEmployeeCode.Name = "textBoxEmployeeCode";
+            this.textBoxEmployeeCode.Size = new System.Drawing.Size(293, 22);
+            this.textBoxEmployeeCode.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(21, 54);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(128, 17);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Enter employee ID:";
+            // 
+            // buttonGetEmployee
+            // 
+            this.buttonGetEmployee.Location = new System.Drawing.Point(336, 74);
+            this.buttonGetEmployee.Name = "buttonGetEmployee";
+            this.buttonGetEmployee.Size = new System.Drawing.Size(199, 23);
+            this.buttonGetEmployee.TabIndex = 14;
+            this.buttonGetEmployee.Text = "Get Employee";
+            this.buttonGetEmployee.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(480, 421);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(198, 23);
+            this.buttonSave.TabIndex = 15;
+            this.buttonSave.Text = "Save Changes";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 548);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonGetEmployee);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxEmployeeCode);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxContry);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxFirstname);
+            this.Controls.Add(this.textBoxId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.TextBox textBoxFirstname;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.TextBox textBoxContry;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxEmployeeCode;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button buttonGetEmployee;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
