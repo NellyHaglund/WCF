e32b79f9d2aa42af83e112ed42d0f63ca63ab23b
Nelly Haglund <haglundnelly@gmail.com>
2016-01-26 11:04:26 +0100

    Query string was wrong, fixed it

--- Laboration_3/NorthwindService/ShipperService.svc.cs
+++ Laboration_3/NorthwindService/ShipperService.svc.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
-using System.Data;
-using System.Data.SqlClient;
 using System.Configuration;
+using System.Data.SqlClient;
 
 namespace NorthwindService
 {
@@ -14,24 +8,24 @@ namespace NorthwindService
     // NOTE: In order to launch WCF Test Client for testing this service, please select ShipperService.svc or ShipperService.svc.cs at the Solution Explorer and start debugging.
     public class ShipperService : IShipperService
     {
-        string _connectionstring = ConfigurationManager.
-    ConnectionStrings["NORTHWND"].ConnectionString;
+        private readonly string _connectionstring = ConfigurationManager.
+            ConnectionStrings["NORTHWND"].ConnectionString;
 
         public Shipper GetShipperById(int id)
         {
             var shipper = new Shipper();
-            string queryString = "SELECT ShipperID, CompanyName, Phone WHERE ShipperID =" + id;
-            using (SqlConnection connection = new SqlConnection(_connectionstring))
+            var queryString = "SELECT ShipperID, CompanyName, Phone FROM dbo.Shippers WHERE ShipperID =" + id;
+            using (var connection = new SqlConnection(_connectionstring))
             {
-                SqlCommand command = new SqlCommand(queryString, connection);
+                var command = new SqlCommand(queryString, connection);
                 connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-             
+                var reader = command.ExecuteReader();
+
                 while (reader.Read())
                 {
-                    shipper.ShipperID = (int)reader["ShipperID"];
-                    shipper.CompanyName = (string)reader["CompanyName"];
-                    shipper.Phone = (string)reader["Phone"];
+                    shipper.ShipperID = (int) reader["ShipperID"];
+                    shipper.CompanyName = (string) reader["CompanyName"];
+                    shipper.Phone = (string) reader["Phone"];
                 }
                 reader.Close();
             }
